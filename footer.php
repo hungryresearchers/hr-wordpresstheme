8418ca4c58ff168f6c62d10495cb672343b05e3f
hiroki <623dgk511522@gmail.com>
2017-08-24 21:26:17 +0900

    Display top_page

--- footer.php
+++ footer.php
@@ -1,34 +1,18 @@
-<?php
-
-    // 各種設定
-    $cf_setting = get_page_by_path('setting');
-    $cf_setting = $cf_setting->ID;
-    $setting_twitter    = get_field('link_twitter', $cf_setting);
-    $setting_line       = get_field('link_line', $cf_setting);
-    $setting_instagram  = get_field('link_instagram', $cf_setting);
-    $setting_facebook   = get_field('link_facebook', $cf_setting);
-
-    $setting_sns_fb   = get_field('sns_fb', $cf_setting);
-    $setting_sns_tw_widget  = get_field('sns_tw_widgets_tag', $cf_setting);
-    $setting_sns_insta_widget  = get_field('instagram_accesstoken', $cf_setting);
-?>
-
-
 <div class="refer_lists">
-<ul class="refer_list">
-    <?php if ("" != $setting_twitter) { ?>
-    <li><a href="https://twitter.com/<?php echo $setting_twitter; ?>" target="_blank" onclick="ga('send','pageview','/twitter');"><img src="<?php echo get_template_directory_uri(); ?>/images/ico_twitter.png" alt=""></a></li>
-    <?php } ?>
-    <?php if ("" != $setting_line) { ?>
-    <li><a href="<?php echo $setting_line; ?>" target="_blank" onclick="ga('send','pageview','/line');"><img src="<?php echo get_template_directory_uri(); ?>/images/ico_line.png" alt=""></a></li>
-    <?php } ?>
-    <?php if ("" != $setting_instagram) { ?>
-    <li><a href="<?php echo $setting_instagram; ?>" target="_blank" onclick="ga('send','pageview','/instagram');"><img src="<?php echo get_template_directory_uri(); ?>/images/ico_instagram.png" alt=""></a></li>
-    <?php } ?>
-    <?php if ("" != $setting_facebook) { ?>
-    <li><a href="<?php echo $setting_facebook; ?>" target="_blank" onclick="ga('send','pageview','/facebook');"><img src="<?php echo get_template_directory_uri(); ?>/images/ico_facebook.png" alt=""></a></li>
-    <?php } ?>
-</ul>
+  <ul class="refer_list">
+      <?php if ("" != $setting_twitter) { ?>
+      <li><a href="https://twitter.com/<?php echo $setting_twitter; ?>" target="_blank" onclick="ga('send','pageview','/twitter');"><img src="<?php echo get_template_directory_uri(); ?>/images/ico_twitter.png" alt=""></a></li>
+      <?php } ?>
+      <?php if ("" != $setting_line) { ?>
+      <li><a href="<?php echo $setting_line; ?>" target="_blank" onclick="ga('send','pageview','/line');"><img src="<?php echo get_template_directory_uri(); ?>/images/ico_line.png" alt=""></a></li>
+      <?php } ?>
+      <?php if ("" != $setting_instagram) { ?>
+      <li><a href="<?php echo $setting_instagram; ?>" target="_blank" onclick="ga('send','pageview','/instagram');"><img src="<?php echo get_template_directory_uri(); ?>/images/ico_instagram.png" alt=""></a></li>
+      <?php } ?>
+      <?php if ("" != $setting_facebook) { ?>
+      <li><a href="<?php echo $setting_facebook; ?>" target="_blank" onclick="ga('send','pageview','/facebook');"><img src="<?php echo get_template_directory_uri(); ?>/images/ico_facebook.png" alt=""></a></li>
+      <?php } ?>
+  </ul>
 </div>
 
 <div id="footer">
@@ -49,14 +33,14 @@
 <a class="window_right2" href="<?php echo home_url('/blog'); ?>" alt=""><img src="<?php echo get_template_directory_uri(); ?>/library/img/btn_blog.png" alt="ブログ"/></a>
 <a id="totop" href="#" alt=""><img src="<?php echo get_template_directory_uri(); ?>/library/img/totop.png" alt="page top"/></a>
 <?php else: ?>
-<table id="sp_bottom">
-  <tr>
-    <td class="sp_totop"><a href="#" alt="">page<br />top</a></td>
-  </tr>
-</table>
+    <table id="sp_bottom">
+      <tr>
+        <td class="sp_totop"><a href="#" alt="">page<br />top</a></td>
+      </tr>
+    </table>
 <?php endif; ?>
 
-</div>
-<?php wp_footer(); ?>
-</body>
+  </div>
+    <?php wp_footer(); ?>
+  </body>
 </html>
--- index.php
+++ index.php
@@ -2,26 +2,21 @@
 /*
 Template Name: トップテンプレート
 */
-?>
 
-<?php get_header(); ?>
+get_header(); ?>
 
+<div class="">
 
-<?php
-	$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-	$args=array(
-		'post_type' => 'page',
-		'p' => '5',
-		'paged' => $paged
-	);
-?>
-<?php query_posts( $args ); ?>
-<?php if(have_posts()): ?>
-<?php while(have_posts()):the_post(); ?>
-<?php the_content(); ?>
-<?php endwhile; else: ?>
-<?php endif; ?>
-<?php wp_reset_query(); ?>
+	<p>test　表示されてるかな？</p>
 
+	<?php query_posts( $args ); ?>
+	<?php if(have_posts()): ?>
+	<?php while(have_posts()):the_post(); ?>
+	<?php the_content(); ?>
+	<?php endwhile; else: ?>
+	<?php endif; ?>
+	<?php wp_reset_query(); ?>
+
+</div>
 
 <?php get_footer(); ?>
